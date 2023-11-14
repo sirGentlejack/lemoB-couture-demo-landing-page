172007a0e19a464d564fa64af3b05fedfbf3e957
sirGentlejack <fagbemiilemobayo@gmail.com>
2023-11-14 01:47:21 +0100

    responsiveness update

---
+++ src/Components/GallerySection.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Gallery from "./Gallery";
+import Data1 from "../Data1";
+
+export default function GallerySection() {
+  return (
+    <div>
+      <h3 className="sub_heading">Gallery</h3>
+      <div className="card_list">
+        {Data1.map((item) => (
+          <Gallery key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
