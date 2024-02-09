8bbc8fe9a9580b4bdb7c1e7acc2d216df58669e7
sirGentlejack <fagbemiilemobayo@gmail.com>
2024-02-09 01:21:18 +0100

    Add firebase dependency

---
+++ src/Components/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvu0-a42oiUt7mLAmzQTBX6ilVRvgkZjs",
+  authDomain: "lemob-couture.firebaseapp.com",
+  projectId: "lemob-couture",
+  storageBucket: "lemob-couture.appspot.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
