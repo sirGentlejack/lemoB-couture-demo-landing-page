c36e8de433ebd4e13a67215c6d158323638d18e1
sirGentlejack <fagbemiilemobayo@gmail.com>
2024-02-27 22:20:36 +0100

    Add private route for blog page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Bottom from "./Components/Bottom";
@@ -17,36 +17,48 @@ import Signin from "./Pages/Signin";
 import Successful from "./sections/Successful";
 import Dashboard from "./Pages/Dashboard";
 import Fashion from "./Components/Fashion";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   return (
     <Router>
-      <div className="App">
-        <Navbar isAuthenticated={isAuthenticated} />
+      <Fragment>
+        <div className="App">
+          <Navbar isAuthenticated={isAuthenticated} />
 
-        {/* Route components */}
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route
-            path="/signin"
-            element={<Signin setIsAuthenticated={setIsAuthenticated} setUser={setUser} />}
-          />
-          <Route path="/dashboard" element={<Dashboard user={user}/>} />
-          <Route path="*" element={<Navigate to="/dashboard" />} />
-          <Route path="/successful" element={<Successful />} />
-          <Route path="*" element={<Navigate to="/successful" />} />
-        </Routes>
+          {/* Route components */}
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/gallery" element={<Gallery />} />
 
-        <Fashion />
-        <Bottom />
-      </div>
+            <Route  path="/" element={<PrivateRoute />}>
+              <Route  path="/blog" element={<Blog />} />
+            </Route>
+
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/signin"
+              element={
+                <Signin
+                  setIsAuthenticated={setIsAuthenticated}
+                  setUser={setUser}
+                />
+              }
+            />
+            <Route path="/dashboard" element={<Dashboard user={user} />} />
+            <Route path="*" element={<Navigate to="/dashboard" />} />
+            <Route path="/successful" element={<Successful />} />
+            <Route path="*" element={<Navigate to="/successful" />} />
+          </Routes>
+
+          <Fashion />
+          <Bottom />
+        </div>
+      </Fragment>
     </Router>
   );
 }
